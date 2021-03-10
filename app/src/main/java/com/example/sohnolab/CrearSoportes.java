597daf8980ba3a4f93b7b865f9fcdaf686cb9eea
Margarita <merinhovega@gmail.com>
2021-03-10 03:09:37 -0500

    Interfaz crear soportes ferminada

---
+++ app/src/main/java/com/example/sohnolab/CrearSoportes.java
@@ -0,0 +1,14 @@
+package com.example.sohnolab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CrearSoportes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_soportes);
+    }
+}
