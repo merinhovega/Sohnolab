c81916f5350116911e97fd06efd0a204c7eee38f
Margarita <merinhovega@gmail.com>
2021-03-10 03:50:55 -0500

    Interfaz detalle soporte terminada

---
+++ app/src/main/java/com/example/sohnolab/DetalleSoporte.java
@@ -0,0 +1,14 @@
+package com.example.sohnolab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetalleSoporte extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_soporte);
+    }
+}
